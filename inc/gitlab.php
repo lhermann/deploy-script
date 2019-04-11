2ee7e4b669300817f8f31b0d1a88ee6c24afed90
Lukas Hermann <lukher6@web.de>
2019-04-11 17:41:23 +0200

    Add some checks for gitlab

--- inc/gitlab.php
+++ inc/gitlab.php
@@ -4,6 +4,11 @@ class Gitlab {
     public $token, $registry, $tag;
 
     function __construct() {
+
+        $keys = array_keys($_POST);
+        if(!(in_array('token', $keys) && in_array('registry', $keys) && in_array('tag', $keys)))
+            exit("Error: Values are missing\n");
+
         $this->token = $_POST["token"];
         $this->registry = $_POST["registry"];
         $this->tag = $_POST["tag"];
